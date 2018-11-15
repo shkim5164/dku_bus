846d01dcbc350dcb06ae3ed3b1e57d56c6550479
shkim5164 <gk457@naver.com>
2018-11-15 08:40:11 +0000

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+     require ('open-uri')
+     #uri를 열기 위한 루비에 자동으로 설치된 잼 같은것. 
+     uri = open("http://m.gbis.go.kr/search/getBusStationArrival.do?stationId=228001737")
+     #uri라는 변수에 버스정보가 담긴 url을 열어서 저장함. 
+     str_uri = uri.read
+     #str_uri라는 변수에 uri변수에 저장한 것을 읽어오도록 저장. 
+     get_info = JSON.parse(str_uri)
+     #get_info라는 변수에 JSON 형식으로 str_uri에 저장된 것을 담아둠. => 해쉬형식으로 담겨짐
+     @bus24 = get_info["busStationArrivalInfo"]["arrivalList"][5]
+     @bus720_3 = get_info["busStationArrivalInfo"]["arrivalList"][2]
+   
+  end
+end
